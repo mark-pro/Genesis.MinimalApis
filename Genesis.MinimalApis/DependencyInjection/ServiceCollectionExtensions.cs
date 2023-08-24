e312f3751e8e316fd95801f59eca9929c1b8f97a
mark-pro <20671988+mark-pro@users.noreply.github.com>
2023-08-23 21:26:20 -0400

    Added comments

--- Genesis.MinimalApis/DependencyInjection/ServiceCollectionExtensions.cs
+++ Genesis.MinimalApis/DependencyInjection/ServiceCollectionExtensions.cs
@@ -7,12 +7,21 @@ public static class ServiceCollectionExtensions {
     public static IServiceCollection RegisterEndpoints<T>(this IServiceCollection services, Func<IServiceProvider, object> func) where T : IEndpoints =>
         services.AddSingleton(typeof(T), func);
 
+    /// <summary>
+    /// Adds a singleton instance of <seealso cref="IEndpoints"/> to the service collection.
+    /// </summary>
     public static IServiceCollection AddSingletonEndpoints<T>(this IServiceCollection services) where T : class, IEndpoints =>
         services.AddSingleton<T>();
 
+    /// <summary>
+    /// Adds a scoped instance of <seealso cref="IEndpoints"/> to the service collection.
+    /// </summary>
     public static IServiceCollection AddScopedEndpoints<T>(this IServiceCollection services) where T : class, IEndpoints =>
         services.AddScoped<T>();
-
+    
+    /// <summary>
+    /// Adds a transient instance of <seealso cref="IEndpoints"/> to the service collection.
+    /// </summary>
     public static IServiceCollection AddTransientEndpoints<T>(this IServiceCollection services) where T : class, IEndpoints =>
         services.AddTransient<T>();
 
